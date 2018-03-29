80332d46bc79a2164ff0c0338bcefe89bd286a38
AWilkes <AWilkes@awilkessp4.bayviewglen.ca>
2018-03-29 15:14:17 -0400

    Fixed names of rooms (hopefully it is better as someone mentioned you arent actually leaving the room, just going to a different section and i dont really want to play around with the other base code that was provided just yet) feel free to ignore or change back. Also got rid of Mr Deslauriers added "comments"

--- Zork/src/com/bayviewglen/zork/Zork.java
+++ Zork/src/com/bayviewglen/zork/Zork.java
@@ -1,13 +1,10 @@
 package com.bayviewglen.zork;
 
-// This is a comment
-
 public class Zork {
 
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.play();
-		// another silly comment
 	}
 
 }
