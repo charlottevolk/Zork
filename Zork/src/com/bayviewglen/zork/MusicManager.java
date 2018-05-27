3b089dd051eeb049d62eac5503ce20fd891af3fe
AWilkes <AWilkes@awilkessp4.bayviewglen.ca>
2018-05-27 15:21:59 -0400

    Added music manager

--- Zork/src/com/bayviewglen/zork/MusicManager.java
+++ Zork/src/com/bayviewglen/zork/MusicManager.java
@@ -1,6 +1,30 @@
 package com.bayviewglen.zork;
 
-public class MusicManager {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MusicManager {
+	private Map<String, MusicClip> clips = new HashMap<>();
+	private MusicClip current;
 	
+	public void playClip(String name) throws Exception{
+		MusicClip musicClip = clips.get(name);
+		if(musicClip == null) {
+			musicClip = new MusicClip(name);
+			clips.put(name, musicClip);
+		}
+		 stopCurrent();
+		 current = musicClip;
+		 musicClip.play();
+	}
+	
+	public void stopPlaying() {
+		stopCurrent();
+	}
+	
+	private void stopCurrent() {
+		if (current != null) {
+			current.stop();
+		}
+	}
 }
